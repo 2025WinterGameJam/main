ff18177068c4c2e64c796749956945e9451abd7c
syareene <soramame3468@icloud.com>
2026-01-02 21:49:25 +0900

    スコア/タイマー管理スクリプト追加

---
+++ Assets/Code/Score/ScoreScript.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))] // TextMeshProコンポーネント(UI用)が必要であることを明示的に指定
+public class ScoreScript : MonoBehaviour
+{
+    [SerializeField] private int m_Score = 0;
+
+    void Start()
+    {
+        ResetScore();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void SetScoreText()
+    {
+        GetComponent<TextMeshProUGUI>().text = m_Score.ToString("D6"); // 6桁のゼロ埋めでスコアを表示
+    }
+
+    public int AddScore(int point)
+    {
+        m_Score += point;
+        // スコア表示を更新
+        SetScoreText();
+        return m_Score;
+    }
+    public int GetScore()
+    {
+        return m_Score;
+    }
+
+    public void ResetScore()
+    {
+        m_Score = 0;
+        // スコア表示を更新
+        SetScoreText();
+    }
+}
---
+++ Assets/Code/Timer/TimerScript.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using TMPro;
+using System;
+
+[RequireComponent(typeof(TextMeshProUGUI))] // TextMeshProコンポーネント(UI用)が必要であることを明示的に指定
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField] private float m_Time = 0f; // タイマーカウント用
+    const float START_TIME = 120f; // 開始時のタイマー時間(120s)
+
+    void Start()
+    {
+        ResetTime();
+    }
+
+    void Update()
+    {
+        // 時間更新
+        m_Time -= Time.deltaTime;
+        // テキストを変更
+        SetTimeText();
+
+        // 任意の時間以下ならテキストのカラーを変更(未実装)
+
+        if(m_Time <= 0.0f)
+        {
+            // 終了処理
+        }
+    }
+    private void SetTimeText()
+    {
+        // mm:ss形式に変換
+        string time_text = string.Format("{0:D2}:{1:D2}",
+            Mathf.FloorToInt(m_Time / 60), // 分
+            Mathf.FloorToInt(m_Time % 60)  // 秒
+        );
+
+        GetComponent<TextMeshProUGUI>().text = time_text;
+    }
+
+    public float GetTime()
+    {
+        return m_Time;
+    }
+
+    public float AddTime(float time)
+    {
+        m_Time += time;
+        return m_Time;
+    }
+
+    public void ResetTime()
+    {
+        m_Time = START_TIME;
+    }
+}
