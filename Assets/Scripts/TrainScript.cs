f35e83b720f787443b3c5e87053680f30c663b9d
ohkawatakuto <ohkawa.takuto@gmail.com>
2026-01-07 12:31:35 +0900

    ・電車が出てきます。 ・左右から出てきます。

---
+++ Assets/Scripts/TrainScript.cs
@@ -0,0 +1,34 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class TrainScript : MonoBehaviour
+{
+    public float destroyPoint;  //このx座標の絶対値を超えたらこれが付いてるオブジェクトは消え去ります。この世から。永遠に。
+    public float speed; //電車のスピード
+    float direction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        direction = gameObject.transform.position.x / gameObject.transform.position.x; //電車の進行方向
+        if(gameObject.transform.position.x > 0 )
+        {
+            direction *= -1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = gameObject.transform.position;
+        pos.x += speed * direction;
+        gameObject.transform.position = pos;
+
+        float despos = Mathf.Abs(pos.x);
+        if(despos >= destroyPoint)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
