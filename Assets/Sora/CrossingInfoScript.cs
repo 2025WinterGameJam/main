cc0864efe6512085c7365fd2c2942c165740610f
syareene <soramame3468@icloud.com>
2026-01-10 12:02:12 +0900

    ツイッターくんでスコアをシェアできる機能の追加

---
+++ Assets/Sora/CrossingInfoScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CrossingInfoScript : MonoBehaviour
+{
+    // CrossingManager見て現在の開閉状態を見る
+    // 一番先頭のは1/3で次のは2/3みたいな感じで表示。
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Sora/ShareXScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+
+public class ShareXScript : MonoBehaviour
+{
+    [SerializeField] private GameObject m_HoverObject;
+
+    public void OnClick()
+    {
+        // 獲得スコア取得
+        int score = ScoreScript.instance.GetScore();
+
+        // シェアURL生成
+        string esctext = UnityWebRequest.EscapeURL("踏み切りシミュレーターで" + score + "円稼ぎました！\nプレイはこちらから: https://unityroom.com/games/crossing_simulator \n");
+        string esctag = UnityWebRequest.EscapeURL("踏み切りシミュレーター #UnityRoom");
+        string url = "https://x.com/intent/tweet?text=" + esctext + "&hashtags=" + esctag;
+
+        // ブラウザで開く
+        Application.OpenURL(url);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // ホバーしたら説明表示
+        m_HoverObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // ホバー外れたら説明非表示
+        m_HoverObject.SetActive(false);
+    }
+}
